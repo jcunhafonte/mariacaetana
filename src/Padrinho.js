ab4f810375791fee7281301bbac91ce018ea00a8
José Cunha Fonte <jose.fonte@cloudbeds.com>
2021-08-28 04:17:04 +0100

    Add padrinho

--- src/Padrinho.js
+++ src/Padrinho.js
@@ -1,9 +1,18 @@
 import './Padrinho.scss';
+import Confetti from 'react-confetti'
+import Baby from './Baby';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      Padrinho
+    <div className="Padrinho">
+      <Confetti numberOfPieces={50}/>
+      <div className="wrapping">
+        <p class="item-1">Tenho uma surpresa para ti…</p>
+        <p class="item-2">Gostava muito que fosses o meu padrinho.</p>
+        <p class="item-3">Aceitas?<br></br><br></br><Link to="/happy" className="button sofia">SIM!</Link><br></br><Link to="/happy" className="button sofia">CLARO QUE SIM!</Link></p>
+      </div>
+      <Baby />
     </div>
   );
 }
